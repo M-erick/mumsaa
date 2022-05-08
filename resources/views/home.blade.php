85334d097ac3cd41839218184b4692baf095d523
eric <61746940+M-erick@users.noreply.github.com>
2022-05-08 22:55:52 +0300

    full screen view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,67 +1,84 @@
 @extends('layouts.app')
 
 @section('content')
-<main class="sm:container sm:mx-auto sm:mt-10">
-    <div class="w-full sm:px-6">
+    <main class="mt-10 sm:container sm:mx-auto sm:mt-10">
+        <div class="w-full ">
 
-        @if (session('status'))
-            <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
+            @if (session('status'))
+                <div class="px-3 py-4 mb-4 text-sm text-green-700 bg-green-100 border border-t-8 border-green-600 rounded"
+                    role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-        <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
+            <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-lg">
 
-           <!-- component -->
-<div class="bg-gray-100 min-h-screen py-32 px-10 ">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6 ">
+                <!-- component -->
+                <div class="min-h-screen px-5 py-32 pt-20 pb-5 ">
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6 ">
 
-    <div class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
-      <img src="https://images.unsplash.com/photo-1627751476653-e954179b174a" alt="" class="rounded-t-lg w-full">
-    <div class="p-6">
-      <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">This is Amazing for people to visit.</h1>
-    <p class="text-gray-700 my-2 hover-text-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quis.</p>
-    </div>
-      </div>
-    <div class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
-      <img src="https://images.unsplash.com/photo-1572431447238-425af66a273b" alt="" class="rounded-t-lg w-full">
-    <div class="p-6">
-      <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">This is Amazing for people to visit.</h1>
-    <p class="text-gray-700 my-2 hover-text-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quis.</p>
-    </div>
-      </div>
-    <div class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
-      <img src="https://images.unsplash.com/photo-1540202404-1b927e27fa8b" alt="" class="rounded-t-lg w-full">
-    <div class="p-6">
-      <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">This is Amazing for people to visit.</h1>
-    <p class="text-gray-700 my-2 hover-text-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quis.</p>
-    </div>
-      </div>
-    <div class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
-      <img src="https://images.unsplash.com/photo-1506790409786-287062b21cfe" alt="" class="rounded-t-lg w-full">
-    <div class="p-6">
-      <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">This is Amazing for people to visit.</h1>
-    <p class="text-gray-700 my-2 hover-text-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quis.</p>
-    </div>
-      </div>
-    <div class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
-      <img src="https://images.unsplash.com/photo-1505881502353-a1986add3762" alt="" class="rounded-t-lg w-full">
-    <div class="p-6">
-      <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">This is Amazing for people to visit.</h1>
-    <p class="text-gray-700 my-2 hover-text-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quis.</p>
-    </div>
-      </div>
-    <div class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
-      <img src="https://images.unsplash.com/photo-1544473244-f6895e69ad8b" alt="" class="rounded-t-lg w-full">
-    <div class="p-6">
-      <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">This is Amazing for people to visit.</h1>
-    <p class="text-gray-700 my-2 hover-text-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quis.</p>
-    </div>
-      </div>
+                        <div
+                            class="container max-w-md mx-auto transition duration-300 rounded-lg shadow-lg hover:shadow-2xl">
+                            <img src="https://images.unsplash.com/photo-1627751476653-e954179b174a" alt=""
+                                class="w-full rounded-t-lg">
+                            <div class="p-6">
+                                <h1
+                                    class="text-xl font-bold text-gray-900 transition duration-200 md:text-1xl hover:text-indigo-600 ">
+                                    This is Amazing for people to visit.</h1>
+                                <p class="my-2 text-gray-700 hover-text-900 ">Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Praesentium quis.</p>
+                            </div>
+                        </div>
+                        <div
+                            class="container max-w-md mx-auto transition duration-300 rounded-lg shadow-lg hover:shadow-2xl">
+                            <img src="https://images.unsplash.com/photo-1572431447238-425af66a273b" alt=""
+                                class="w-full rounded-t-lg">
+                            <div class="p-6">
+                                <h1
+                                    class="text-xl font-bold text-gray-900 transition duration-200 md:text-1xl hover:text-indigo-600 ">
+                                    This is Amazing for people to visit.</h1>
+                                <p class="my-2 text-gray-700 hover-text-900 ">Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Praesentium quis.</p>
+                            </div>
+                        </div>
+                        <div
+                            class="container max-w-md mx-auto transition duration-300 rounded-lg shadow-lg hover:shadow-2xl">
+                            <img src="https://images.unsplash.com/photo-1540202404-1b927e27fa8b" alt=""
+                                class="w-full rounded-t-lg">
+                            <div class="p-6">
+                                <h1
+                                    class="text-xl font-bold text-gray-900 transition duration-200 md:text-1xl hover:text-indigo-600 ">
+                                    This is Amazing for people to visit.</h1>
+                                <p class="my-2 text-gray-700 hover-text-900 ">Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Praesentium quis.</p>
+                            </div>
+                        </div>
 
-      </div>
-  </div>
-        </section>
-    </div>
-</main>
+                    </div>
+                    <div class="container transition duration-300 rounded-lg shadow-lg max-w-screen-2xl hover:shadow-2xl">
+                        {{-- <img src="images/money.jpg" alt="" class="w-full rounded-t-lg"> --}}
+                        <div class="p-6">
+                            <h1
+                                class="text-xl font-bold text-gray-900 transition duration-200 md:text-1xl hover:text-indigo-600 ">
+                                News/Admin Panel.</h1>
+                            <p class="my-2 text-gray-700 hover-text-900 ">Administration Notification Panel .<br/>
+                            Admin panel</p>
+                        </div>
+                    </div>
+
+                    <div class="container transition duration-300 rounded-lg shadow-lg max-w-screen-2xl hover:shadow-2xl">
+                        {{-- <img src="images/money.jpg" alt="" class="w-full rounded-t-lg"> --}}
+                        <div class="p-6">
+                            <h1
+                                class="text-xl font-bold text-gray-900 transition duration-200 md:text-1xl hover:text-indigo-600 ">
+                                News/Admin Panel.</h1>
+                            <p class="my-2 text-gray-700 hover-text-900 ">Activities .<br/></p>
+
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </div>
+    </main>
 @endsection
