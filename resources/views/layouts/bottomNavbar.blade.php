1f7ddf1a8cc893ffd1584bc0eb3a47c641c92fdf
eric <61746940+M-erick@users.noreply.github.com>
2022-05-11 09:23:34 +0300

    Add font awesome icons ie(home icon,more icons)

---
+++ resources/views/layouts/bottomNavbar.blade.php
@@ -0,0 +1,8 @@
+<div class=" bg-blue-100 pt-10 container    mx-auto flex  justify-start text-white  ">
+    <a class="pl-5   hyper-style pr-2 " href="{{ route('home') }} ">
+        <i class="fa-primary-red fa-home" style="color:red" ></i></a>
+
+    <a class="no-underline hyper-style hover:underline active:text-orange-200 " href="{{ route('list') }} ">LIST</a>
+    {{-- <a class="no-underline  hyper-style" href="{{ route('sell') }} ">SELL</a> --}}
+
+</div>
