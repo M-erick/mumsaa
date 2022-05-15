81b3e696328be3b47e8033e4b25bfc35d5191557
eric <61746940+M-erick@users.noreply.github.com>
2022-05-15 22:38:48 +0300

    changed notify seller to notify member .The button should send a message to the member once clicked by admin

--- resources/views/paymentPanel.blade.php
+++ resources/views/paymentPanel.blade.php
@@ -8,7 +8,7 @@
                 {{-- bid success message --}}
                 @if (session()->has('message'))
                     <div class="w-full m-auto mt-10 p-auto">
-                        <p class="w-full mb-4 text-gray-50 bg-gray-500 rounded-2xl py-4">
+                        <p class="w-full mb-4 text-gray-50 bg-gray-500 rounded-2xl py-4 px-3">
                             {{ session()->get('message') }}
 
                         </p>
@@ -22,7 +22,7 @@
                     <div class="w-4/5 self-center m-auto p-6">
                         <ul>
                             @foreach ($errors->all() as $error)
-                                <li class="w-full mb-4 text-gray-50 bg-red-700 rounded-2xl  p-4">
+                                <li class="w-full mb-4 text-gray-50 bg-red-700 rounded-2xl py-4 px-3">
 
                                     {{ $error }}
                                 </li>
@@ -50,15 +50,9 @@
                 </div>
             </div>
 
-
             <div class="container w-full   bg-white border-t border-b border-gray-300 ">
-
-
-
-
-                <div>
-
-                    <!-- component -->
+               <div>
+                   <!-- component -->
                     <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
 
                         <div
@@ -79,18 +73,7 @@
                                         <th
                                             class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
                                             AMOUNT</th>
-                                        {{-- <th
-                                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
-                                            USERNAME</th> --}}
-                                        {{-- <th
-                                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
-                                            Email</th> --}}
-                                        {{-- <th
-                                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
-                                            NAME</th>
-                                            <th
-                                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
-                                            NAME</th> --}}
+
 
                                         <th
                                             class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
@@ -137,19 +120,14 @@
                                                         </div>
                                                     </div>
                                                 </td>
-                                                {{-- <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-                                                    <div class="text-sm leading-5 text-blue-900">{{ $data->name }}</div>
-                                                </td> --}}
-                                                {{-- <td
-                                                    class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
-                                                    {{ Auth::user()->phone_number }}</td> --}}
+
                                                 <td
                                                     class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
                                                     <span
                                                         class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
                                                         <span aria-hidden
                                                             class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                                        <span class="relative text-xs">active</span>
+                                                        <span class="relative text-xs">{{ $data->payment}}</span>
                                                     </span>
                                                 </td>
                                                 <td
@@ -159,8 +137,8 @@
                                                     class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5">
                                                     <button
                                                         class="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Notify
-                                                        Seller</button>
-                                                </td>
+                                                        Member</button>
+
                                             </tr>
                                             {{-- @endif --}}
                                     @endforeach
