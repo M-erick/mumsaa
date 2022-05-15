05b69c876177e9cf7db2f90de65eb8844da18354
eric <61746940+M-erick@users.noreply.github.com>
2022-05-15 22:47:09 +0300

    inactivate bottomNavbar

--- resources/views/layouts/bottomNavbar.blade.php
+++ resources/views/layouts/bottomNavbar.blade.php
@@ -7,6 +7,9 @@
 
         <a class="no-underline hyper-style hover:underline active:text-orange-200 "
             href="{{ route('list') }} ">LIST</a>
+            <a class="no-underline hyper-style hover:underline active:text-orange-200 "
+            href="{{ route('list') }} ">REGISTRATIONS</a>
+           
         {{-- <a class="no-underline  hyper-style" href="{{ route('sell') }} ">SELL</a> --}}
 
     </div>
